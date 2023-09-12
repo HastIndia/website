2422bba67eff7f21485afcc43918258518e778ef
narendra <51484021+alenway@users.noreply.github.com>
2023-09-13 05:13:17 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Footer from "./component/Footer";
 import Header from "./component/Header";
 import Home from "./pages/Home";
-import Layout from "./pages/Layout";
 
 function App() {
   return (
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,9 +2,10 @@ import { onAuthStateChanged, signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../firebaseconfig";
+import { BsBugFill } from "react-icons/bs";
 
 const Header = () => {
-  const [isloggedin,setIsLoggedIn]=useState(false);
+  const [isloggedin, setIsLoggedIn] = useState(false);
   const userSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -31,29 +32,31 @@ const Header = () => {
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
-          </svg>
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <BsBugFill />
         </a>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <Link to="/" className="mr-5 hover:text-gray-900">Home</Link>
-          <Link to="/cart" className="mr-5 hover:text-gray-900">Cart</Link>
-          <Link to="/contactus" className="mr-5 hover:text-gray-900">Contact Us</Link>
-          <Link to="/payment" className="mr-5 hover:text-gray-900">Payment</Link>
-          {isloggedin?<p onClick={userSignOut}>logout</p>:<Link to="/login" className="mr-5 hover:text-gray-900">Login</Link>}
+          <Link to="/" className="mr-5 hover:text-white">
+            Home
+          </Link>
+          <Link to="/cart" className="mr-5 hover:text-white">
+            Cart
+          </Link>
+          <Link to="/contactus" className="mr-5 hover:text-white">
+            Contact Us
+          </Link>
+          <Link to="/payment" className="mr-5 hover:text-white">
+            Payment
+          </Link>
+          {isloggedin ? (
+            <p onClick={userSignOut}>logout</p>
+          ) : (
+            <Link to="/login" className="mr-5 hover:text-white">
+              Login
+            </Link>
+          )}
         </nav>
-        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-          Button
+        <button className="inline-flex items-center text-white bg-[#884210] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 hover:text-black rounded text-base mt-4 md:mt-0">
+          Shop Art
           <svg
             fill="none"
             stroke="currentColor"
