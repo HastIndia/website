37534d583e80b4920a652c52f367b566d247b44c
narendra <51484021+alenway@users.noreply.github.com>
2023-09-15 01:43:29 +0530

    home page done

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -30,32 +30,11 @@ const Header = () => {
     };
   }, []);
   return (
-    <header className="text-gray-600 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <header className="text-gray-600 body-font bg-[#a35c31]">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row justify-between items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
           <BsBugFill />
         </a>
-        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <Link to="/" className="mr-5">
-            <BtnEffect link="Home" />
-          </Link>
-          <Link to="/cart" className="mr-5">
-            <BtnEffect link="Cart" />
-          </Link>
-          <Link to="/contactus" className="mr-5">
-            <BtnEffect link="Contact Us" />
-          </Link>
-          <Link to="/payment" className="mr-5">
-            <BtnEffect link="Payment" />
-          </Link>
-          {isloggedin ? (
-            <p onClick={userSignOut}>logout</p>
-          ) : (
-            <Link to="/login" className="mr-5">
-              <BtnEffect link="Login" />
-            </Link>
-          )}
-        </nav>
         <button className="inline-flex items-center text-white bg-[#884210] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 hover:text-black rounded text-base mt-4 md:mt-0">
           Shop Art
           <svg
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,140 +1,31 @@
-import { onAuthStateChanged } from "firebase/auth";
-import React, { useEffect, useState } from "react";
-import { auth } from "../firebaseconfig";
-import art1 from "../assets/art1.jpg";
-import art2 from "../assets/art2.jpg";
-import art3 from "../assets/art3.jpg";
-import art4 from "../assets/art4.jpg";
+import "./image.css";
 
 const Home = () => {
   return (
     <>
-      <section className="text-gray-600 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              are you looking for
-              <br className="hidden lg:inline-block" />
-              aesthetic handicraft
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
-              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
-              park mlkshk tote bag selvage hot chicken authentic tumeric
-              truffaut hexagon try-hard chambray.
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex items-center text-white bg-[#884210] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 hover:text-black rounded text-base mt-4 md:mt-0">
-                Buy Now
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  className="w-4 h-4 ml-1"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M5 12h14M12 5l7 7-7 7" />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-            <img
-              className="object-cover object-center rounded"
-              alt="hero"
-              style={{ height: "500px", width: "500px" }}
-              src={art1}
-            />
-          </div>
+      <div className="HomeBg">
+        <div className="px-12">
+          <p className="text-white text-3xl py-3 opacity-40">
+            Looking for <br /> Aesthetic Handicrafts?
+          </p>
+          <a href="mailto:admin@hastindia.com">
+            <button className="inline-flex items-center text-white bg-[#884210] border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+              Contact Us
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                className="w-4 h-4 ml-1"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7" />
+              </svg>
+            </button>
+          </a>
         </div>
-      </section>
-
-      <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto flex flex-wrap">
-          <div className="flex flex-wrap md:-m-2 -m-1">
-            <div className="flex flex-wrap w-1/2">
-              <div className="md:p-2 p-1 w-1/2">
-                <div className="relative">
-                  <img
-                    alt="gallery"
-                    className="w-full object-cover h-full object-center block opacity-70"
-                    src={art2}
-                  />
-                  <div className="absolute inset-0 flex items-center justify-center bg-opacity-60 text-white text-2xl font-bold">
-                  <div className="flex justify-center align-middle">
-                  Comming Soon
-                  </div>
-                  </div>
-                </div>
-              </div>
-              <div className="md:p-2 p-1 w-1/2">
-                <div className="relative">
-                  <img
-                    alt="gallery"
-                    className="w-full object-cover h-full object-center block opacity-70"
-                    src={art3}
-                  />
-                  <div className="absolute inset-0 flex items-center justify-center bg-opacity-60 text-white text-2xl font-bold">
-                    Comming Scoon
-                  </div>
-                </div>
-              </div>
-              <div className="md:p-2 p-1 w-full">
-                <div className="relative">
-                  <img
-                    alt="gallery"
-                    className="w-full h-full object-cover object-center block opacity-70"
-                    src={art4}
-                  />
-                  <div className="absolute inset-0 flex items-center justify-center bg-opacity-60 text-white text-2xl font-bold">
-                    Comming Scoon
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-wrap w-1/2">
-              <div className="md:p-2 p-1 w-full">
-                <div className="relative">
-                  <img
-                    alt="gallery"
-                    className="w-full h-full object-cover object-center block opacity-70"
-                    src={art4}
-                  />
-                  <div className="absolute inset-0 flex items-center justify-center bg-opacity-60 text-white text-2xl font-bold">
-                    Comming Scoon
-                  </div>
-                </div>
-              </div>
-              <div className="md:p-2 p-1 w-1/2">
-                <div className="relative">
-                  <img
-                    alt="gallery"
-                    className="w-full object-cover h-full object-center block opacity-70"
-                    src={art2}
-                  />
-                  <div className="absolute inset-0 flex items-center justify-center bg-opacity-60 text-white text-2xl font-bold">
-                    Comming Scoon
-                  </div>
-                </div>
-              </div>
-              <div className="md:p-2 p-1 w-1/2">
-                <div className="relative">
-                  <img
-                    alt="gallery"
-                    className="w-full object-cover h-full object-center block opacity-70"
-                    src={art2}
-                  />
-                  <div className="absolute inset-0 flex items-center justify-center bg-opacity-60 text-white text-2xl font-bold">
-                    Comming Scoon
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      </div>
     </>
   );
 };
